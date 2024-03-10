998ece3aeac40436bf75d5284c39e5972ed67103
ChuckleChic <priya18jha08@gmail.com>
2024-03-10 12:26:22 +0530

    1

--- App/urls.py
+++ App/urls.py
@@ -5,7 +5,7 @@ from App import views
 urlpatterns = [
     path("", views.index , name='home'),
     path("about", views.about , name='about'),
-    path("receiver", views.Executive , name='receiver'),
+    path("receiver", views.receiver , name='receiver'),
      path("donor", views.donor , name='donor'),
     path("services", views.services , name='services'),
     path("contact", views.contact , name='contact'),
